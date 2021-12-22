913bbed929b5785e5c96f818cab950852912db89
Lukas Valta <valtalukas@seznam.cz>
2021-12-22 12:59:48 +0100

    haha

--- functions/sendGridMedZdarma.js
+++ functions/sendGridMedZdarma.js
@@ -1,34 +1,34 @@
-const sgMail = require('@sendgrid/mail');
+const sgMail = require("@sendgrid/mail")
 
-exports.handler = async (event) => {
-  const body = JSON.parse(event.body);
+exports.handler = async event => {
+  const body = JSON.parse(event.body)
 
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
   const downloadEmailVariables = {
     to: body.email,
     from: {
-      email: ' veronika.fraitova@seznam.cz',
-      name: 'OpenYourEyes',
+      email: " veronika.fraitova@seznam.cz",
+      name: "OpenYourEyes",
     },
-    templateId: 'd-fb8d783ee6c148688a41f1226c467e46',
+    templateId: "d-fb8d783ee6c148688a41f1226c467e46",
     dynamic_template_data: {
       url: body.url,
     },
-  };
-
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method not Allowed' };
   }
-  await sgMail.send(downloadEmailVariables).then(() => {}, console.error);
+
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method not Allowed" }
+  }
+  await sgMail.send(downloadEmailVariables).then(() => {}, console.error)
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-      'Access-Control-Allow-Headers': 'Content-Type',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type",
     },
-    body: 'Sucessfully',
-  };
-};
+    body: "Sucessfully",
+  }
+}
--- functions/stripe.js
+++ functions/stripe.js
@@ -1,14 +1,14 @@
-const stripe = require('stripe')('sk_live_4dOYIPZGiNMcgi4tEvhhTgyD00y4xofpmU');
+const stripe = require("stripe")("sk_live_4dOYIPZGiNMcgi4tEvhhTgyD00y4xofpmU")
 
-exports.handler = async (event) => {
-  const { name, price, url, img } = JSON.parse(event.body);
-  metadata = JSON.stringify({ url: url, name: name });
+exports.handler = async event => {
+  const { name, price, url, img } = JSON.parse(event.body)
+  metadata = JSON.stringify({ url: url, name: name })
   const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
+    payment_method_types: ["card"],
     line_items: [
       {
         price_data: {
-          currency: 'czk',
+          currency: "czk",
           product_data: {
             name: name,
           },
@@ -19,57 +19,20 @@ exports.handler = async (event) => {
     ],
     payment_intent_data: { metadata: { name: name, url: url } },
 
-    mode: 'payment',
-    success_url: 'https://openyoureyes.cz/thanks',
-    cancel_url: 'https://openyoureyes.cz/eshop',
-  });
+    mode: "payment",
+    success_url: "https://openyoureyes.cz/thanks",
+    cancel_url: "https://openyoureyes.cz/eshop",
+  })
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-      'Access-Control-Allow-Headers': 'Content-Type',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type",
     },
     body: JSON.stringify({
       id: session.id,
     }),
-  };
-};
-
-/* 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-export default async function handler(req, res) {
-  const { name, price, url, img } = JSON.parse(req.body);
-  metadata = JSON.stringify({ url: url, name: name });
-  if (req.method === 'POST') {
-    try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price_data: {
-              currency: 'czk',
-              product_data: {
-                name: name,
-              },
-              unit_amount: price * 100,
-            },
-            quantity: 1,
-          },
-        ],
-        payment_intent_data: { metadata: { name: name, url: url } },
-        mode: 'payment',
-        success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/meditace`,
-      });
-      res.redirect(303, session.url);
-    } catch (err) {
-      res.status(err.statusCode || 500).json(err.message);
-    }
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
   }
-} */
+}
